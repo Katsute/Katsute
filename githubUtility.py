c9a68a4fc9de9aba1f941ec5d1c07b90095f7601
Katsute <58778985+Katsute@users.noreply.github.com>
2020-11-20 22:00:46 -0500

    Optimize@9a2033f (#3)
    * simplified file
    
    * simplified readme
    
    * resolve conflicts
    
    * resolve conflicts

--- githubUtility.py
+++ githubUtility.py
@@ -6,14 +6,8 @@ import re
 from github.GithubException import UnknownObjectException
 
 
-def getStatistics(config):
-    gh_user = config['gh_user']
-    boolean_includePrivate = config['include_private']
-    arr_hideLang = config['hide_lang']
-    int_maxLang = config['max_lang']
-    ignore_repo = config['ignore_repo']
-
-    int_user_id = gh_user.id
+def get_statistics(user, private, hide_languages, max_languages, hide_repos):
+    int_user_id = user.id
 
     date_init_year = datetime.datetime.combine(datetime.date(datetime.date.today().year, 1, 1), datetime.time.min)
 
@@ -25,26 +19,26 @@ def getStatistics(config):
         "contributions": 0
     }
 
-    for repo in gh_user.get_repos():
-        if repo.id in ignore_repo or (repo.private and not boolean_includePrivate):
+    for repo in user.get_repos():
+        if repo.id in hide_repos or (repo.private and not private):
             continue
 
         # contributions
         if repo.owner.id != int_user_id:
             map_stats['contributions'] += 1  # no ++ :(
         # issues
-        map_stats['issues'] += repo.get_issues(since=date_init_year, state="all", creator=gh_user.name).totalCount
+        map_stats['issues'] += repo.get_issues(since=date_init_year, state="all", creator=user.name).totalCount
         # pulls
         for pull in repo.get_pulls(state="all"):
-            if pull.user.id == gh_user.id and pull.updated_at >= date_init_year:
+            if pull.user.id == user.id and pull.updated_at >= date_init_year:
                 map_stats['pulls'] += 1
         # commits
-        map_stats['commits'] += repo.get_commits(since=date_init_year, author=gh_user.name).totalCount
+        map_stats['commits'] += repo.get_commits(since=date_init_year, author=user.name).totalCount
 
         # language statistics
-        double_contrib = repo.get_commits(author=gh_user.name).totalCount / repo.get_commits().totalCount  # what % of commits is made by this user
+        double_contrib = repo.get_commits(author=user.name).totalCount / repo.get_commits().totalCount  # what % of commits is made by this user
         for lang, count in repo.get_languages().items():
-            if not (lang in arr_hideLang):  # remove hidden config
+            if not (lang in hide_languages):  # remove hidden config
                 if not (lang in map_lang_contrib.keys()):
                     map_lang_contrib[lang] = count * double_contrib
                 else:
@@ -57,7 +51,7 @@ def getStatistics(config):
 
     index = 0
     for i in map_lang_contrib:
-        if index < int_maxLang:
+        if index < max_languages:
             map_lang.append({
                 "name": i[0],
                 "percent": int((float(i[1]) * 100 / double_total) * 100) / 100  # as two point float
